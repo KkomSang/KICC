9c21ad59e009e65a8cd35467af33eea07224eb6b
sangeun <olsang1203@gmail.com>
2026-05-04 16:45:56 +0200

    feat: implement img upload

--- src/main/java/kicc/cicd/web/controller/DiaryController.java
+++ src/main/java/kicc/cicd/web/controller/DiaryController.java
@@ -7,15 +7,18 @@ import kicc.cicd.web.dto.diaryDTO.DiaryRequestDTO;
 import kicc.cicd.web.dto.diaryDTO.DiaryResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/diaries")
 @RequiredArgsConstructor
 public class DiaryController {
     private final DiaryService diaryService;
-    @PostMapping
-    public ApiResponse<DiaryResponseDTO.DiaryIdDTO> createDiary(@RequestBody DiaryRequestDTO.DiaryDTO request) {
-        return ApiResponse.of(SuccessStatus._DIARY_CREATE_OK, diaryService.createDiary(request));
+    @PostMapping(consumes = "multipart/form-data")
+    public ApiResponse<DiaryResponseDTO.DiaryIdDTO> createDiary(@RequestPart DiaryRequestDTO.DiaryDTO request, @RequestPart(required = false) List<MultipartFile> images) {
+        return ApiResponse.of(SuccessStatus._DIARY_CREATE_OK, diaryService.createDiary(request, images));
     }
     @GetMapping
     public ApiResponse<DiaryResponseDTO.DiaryPreviewListDTO> getDiaryList(@RequestParam(value = "page", defaultValue = "1")Integer page) {
