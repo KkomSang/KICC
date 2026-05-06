987aa1bef3a41d01b004228a7f2b0a05e630724e
sangeun <olsang1203@gmail.com>
2026-05-06 13:00:48 +0200

    fix: fix health check path

--- src/main/java/kicc/cicd/web/controller/HealthCheckController.java
+++ src/main/java/kicc/cicd/web/controller/HealthCheckController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-    @GetMapping("/health")
+    @GetMapping
     public String healthCheck() {
-        return "I'm healthy";
+        return "I'm healthy✅";
     }
 }
