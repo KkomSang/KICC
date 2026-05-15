69c5ef1ca33d91330e439a3b8402ba99494b195a
sangeun <olsang1203@gmail.com>
2026-05-15 21:39:10 +0200

    style: to v1

--- src/main/java/kicc/cicd/web/controller/DiaryViewController.java
+++ src/main/java/kicc/cicd/web/controller/DiaryViewController.java
@@ -10,40 +10,40 @@ public class DiaryViewController {
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
     /*V1*/
-//    @GetMapping("/diary")
-//    public String diaryList() {
-//        return "diary-list-v1";
-//    }
-//
-//    @GetMapping("/diary/{id}")
-//    public String diaryDetail(@PathVariable Long id, Model model) {
-//        model.addAttribute("id", id);
-//        return "diary-detail-v1";
-//    }
-//
-//    @GetMapping("/diary/write")
-//    public String diaryWrite() {
-//        return "diary-write-v1";
-//    }
-
-
-
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-    /*V2*/
     @GetMapping("/diary")
     public String diaryList() {
-        return "diary-list-v2";
+        return "diary-list-v1";
     }
 
     @GetMapping("/diary/{id}")
     public String diaryDetail(@PathVariable Long id, Model model) {
         model.addAttribute("id", id);
-        return "diary-detail-v2";
+        return "diary-detail-v1";
     }
 
     @GetMapping("/diary/write")
     public String diaryWrite() {
-        return "diary-write-v2";
+        return "diary-write-v1";
     }
+
+
+
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    /*V2*/
+//    @GetMapping("/diary")
+//    public String diaryList() {
+//        return "diary-list-v2";
+//    }
+//
+//    @GetMapping("/diary/{id}")
+//    public String diaryDetail(@PathVariable Long id, Model model) {
+//        model.addAttribute("id", id);
+//        return "diary-detail-v2";
+//    }
+//
+//    @GetMapping("/diary/write")
+//    public String diaryWrite() {
+//        return "diary-write-v2";
+//    }
 
 }
